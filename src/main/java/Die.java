61ecbc161fc05ef8056570928b90ed91c232a4a2
Joshua Gomes da Costa <joshua.gomesdacosta@heig-vd.ch>
2019-04-12 14:19:57 +0200

    MGame et Die basiques avec leur classe test

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,2 +1,14 @@
+import java.util.Random;
+
 public class Die {
+    private final Random rand = new Random();
+    protected final int maxFace = 6;
+
+    public Die(){
+
+    }
+
+    public int getFaceValue(){
+        return rand.nextInt(maxFace);
+    }
 }
--- src/main/java/MGame.java
+++ src/main/java/MGame.java
@@ -1,2 +1,17 @@
+
+
 public class MGame {
+    private final Board board = new Board();
+    private Die faceValue;
+    private Player player;
+    private final int roundCnt
+
+
+    public MGame(){
+
+    }
+
+    public void playGame(){
+
+    }
 }
