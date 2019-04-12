28a1b3006040bd6c123038b543b907045d0eb74e
Boosterloop <alison.savary@me.com>
2019-04-12 14:38:12 +0200

    Board and BoardTest

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,3 +1,21 @@
+import java.util.LinkedList;
+
 public class Board {
+    private LinkedList<Square> squares;
+    private final int NB_SQUARES = 40;
+
+    public Board() {
+        squares = new LinkedList<>();
+        // Depart
+        squares.add(new Square("Départ"));
+
+        for(int i = 1; i < NB_SQUARES; i++) {
+            squares.add(new Square("Square " + i));
+        }
+    }
+
+    public LinkedList<Square> getSquares() {
+        return squares;
+    }
 
 }
