dea90caa966fe71e271c866e4199a78ebb90a1da
Joshua Gomes da Costa <joshua.gomesdacosta@heig-vd.ch>
2019-04-29 14:43:09 +0200

    why

--- src/main/java/MGame.java
+++ src/main/java/MGame.java
@@ -1,17 +1,31 @@
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class MGame {
     private final Board board = new Board();
-    private Die faceValue;
-    private Player player;
+    private Die[] dice = new Die[2];
+    private List<Player> players = new ArrayList<Player>();
     private final int roundCnt = 20;
 
+    public MGame(String[] playersName) throws Exception{
+        if(playersName.length < 2)
+            throw new Exception("pas assez de joueurs");
 
-    public MGame(){
-
+        for(String name: playersName){
+            players.add(new Player(name, dice, board));
+        }
     }
 
     public void playGame(){
+        for(int i = 0; i < roundCnt; i++){
+            playRound();
+        }
+    }
 
+    private void playRound(){
+        for(Player player : players){
+            player.takeTurn();
+        }
     }
 }
--- src/test/java/MGameTest.java
+++ src/test/java/MGameTest.java
@@ -1,10 +1,45 @@
 
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MGameTest {
     @Test
-    public void itShouldWork(){
-        assertTrue(true);
+    public void itShouldWorkBetween2and8Players() {
+        int i = 2;
+        try{
+
+            List<String> players = new ArrayList<String>();
+            players.add("joueur1");
+            players.add("joueur2");
+            for (i = 2; i < 8; i++) {
+                players.add("joueur" + i);
+                new MGame(players.toArray(new String[]{}));
+            }
+        }
+        catch(Exception e){
+            if(e.getMessage() == "pas assez de joueurs" || e.getMessage() == "trop de joueurs"){
+                fail(i +" joeurs");
+            }
+        }
+
+        try {
+            new MGame(new String[]{"joueur1"});
+            fail("only 1 player");
+        }
+         catch(Exception e){
+            assertTrue(true);
+        }
+        try{
+            new MGame(new String[]{"joueur1","joueur2","joueur3","joueur4","joueur5","joueur6","joueur7","joueur8","joueur9"});
+            fail("too mush players(9)");
+        }
+        catch(Exception e){
+            assertTrue(true);
+        }
     }
 }
